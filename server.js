932d2ed3bcaf084d89ddc336895a236493ffaf41
Emil Koul <dikasi@hotmail.com>
2025-08-30 22:26:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.send('Hello from my OpenShift Node.js app!');
+});
+
 app.get('/hello', (req, res) => {
   res.send('Hello from OpenShift!');
 });
